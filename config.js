371b37212e387bb917bb0c5e17cf2b72461d0242
Purva Adke <padke@twilio.com>
2017-09-30 11:29:04 -0500

    first

---
+++ config.js
@@ -0,0 +1,22 @@
+
+var dotenv = require('dotenv');
+var result = dotenv.config({path: '.env'});
+// Define app configuration in a single location, but pull in values from
+// system environment variables (so we don't check them in to source control!)
+
+// console.log(process.env.TWILIO_ACCOUNT_SID);
+// console.log(result);
+module.exports = {
+    // Twilio Account SID - found on your dashboard
+    accountSid: process.env.TWILIO_ACCOUNT_SID,
+
+    // Twilio Auth Token - found on your dashboard
+    authToken: process.env.TWILIO_AUTH_TOKEN,
+
+    // A Twilio number that you have purchased through the twilio.com web
+    // interface or API
+    twilioNumber: process.env.TWILIO_NUMBER,
+
+    // The port your web application will run on
+    port: process.env.PORT || 3000,
+};
